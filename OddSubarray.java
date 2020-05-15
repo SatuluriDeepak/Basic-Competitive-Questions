b30e10c5fcb17c9d13831e8ec170deaeeb952b3f
SatuluriDeepak <65010016+SatuluriDeepak@users.noreply.github.com>
2020-05-15 11:47:24 +0530

    Update OddSubarray.java

--- OddSubarray.java
+++ OddSubarray.java
@@ -1,37 +1,45 @@
-import java.util.Scanner;
 
-public class OddSubarray {
-	public static void main(String...args) {
-			Scanner sc= new Scanner(System.in);
-			System.out.println("Enter Length : ");
-			long n=sc.nextLong();
-			long [] arr= new long[(int) n];
-			for(long i=0;i<n;i++) 
-				arr[(int) i]=sc.nextLong();			
-			subarray(arr);
-	}
-	static void subarray(long[] arr) {
-		long even = 0,odd = 0,c=0;
-		for(long i=0;i<arr.length;i++) {
-			for(long j=i;j<arr.length;j++) {
-				for(long k=i;k<=j;k++) {
-					//System.out.print(arr[(int) k]+" ");
-					{
-						if(arr[(int)k]%2==0) 
-							even+=1;
-						else 
-							odd+=1;	
-					}
-					if(even==odd){
-						c+=1;
-						even=0;
-						odd=0;
-					}	
-				}
-			}
-		}
-		System.out.println(c);
+class OddSubarray { 
+	static int countSubarrays(int[] arr, 
+			int n) { 
+	 
+		int difference = 0; 
+		int ans = 0; 
+
+	 
+		int[] hash_positive = new int[n + 1]; 
+		int[] hash_negative = new int[n + 1]; 
+		hash_positive[0] = 1; 
+
 		
-	}
+		for (int i = 0; i < n; i++) { 
+			
+			if ((arr[i] & 1) == 1) { 
+				difference++; 
+			} else{ 
+				difference--; 
+			} 
 
-}
+	
+			if (difference < 0) { 
+				//System.out.println(-difference);
+				//System.out.println(hash_negative[-difference]);
+				ans += hash_negative[-difference]; 
+				hash_negative[-difference]++; 
+			}
+			else { 
+				ans += hash_positive[difference]; 
+				hash_positive[difference]++; 
+			} 
+		} 
+		return ans; 
+	} 
+	public static void main(String[] args) { 
+		int[] arr = new int[]{3, 4, 6, 8, 
+			1, 10, 5, 7}; 
+		int n = arr.length; 
+
+		System.out.println(countSubarrays(arr, n)); 
+	} 
+} 
+
